bfa40a879bbc4912784c6ce01e21a5eaf5f2e9c9
Arvin Bohlool <abohloolk@gmail.com>
2025-01-29 10:35:02 +0330

    Event Handler Added

--- pygame1.py
+++ pygame1.py
@@ -1,9 +1,29 @@
 import pygame
+from pygame.locals import *
 
 pygame.init()
 
-HEIGHT = 500
 WIDTH = 500
+HEIGHT = 500
 
 screen = pygame.display.set_mode((HEIGHT, WIDTH))
 pygame.display.set_caption("First Game")
+
+x = 50
+y = 50
+width = 40
+height = 60
+velocity = 5
+
+running = True
+while running:
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == KEYUP:
+            if event.key == K_ESCAPE:
+                running = False
+    
+pygame.quit()
