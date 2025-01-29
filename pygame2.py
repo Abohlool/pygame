eb04749566586428d9c2dbf55e66e25a4baed3bb
Arvin Bohlool <abohloolk@gmail.com>
2025-01-29 11:24:09 +0330

    constants

--- pygame2.py
+++ pygame2.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from random import randint, choice
 
 pygame.init()
 
@@ -9,6 +10,13 @@ HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Animation")
 
+# direction constants
+DOWNLEFT = "downleft"
+DOWNRIGHT = "downright"
+UPLEFT = "upleft"
+UPRIGHT = "upright"
+
+velocity = 5
 
 
 running = True
@@ -16,6 +24,12 @@ while (running):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+        if event.type == KEYUP:
+            if event.key == K_ESCAPE:
+                running = False
+                
+        
 
 
 pygame.quit
