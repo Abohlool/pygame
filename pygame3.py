b428bb4a44501bffb68cc807d442dba1be0e64e2
Arvin Bohlool <abohloolk@gmail.com>
2025-01-29 12:25:33 +0330

    consts, player, & points

--- pygame3.py
+++ pygame3.py
@@ -10,6 +10,22 @@ HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Collision Detection")
 
+counter = 0
+NEWPOINT = 40
+SIZE = 20
+
+player = pygame.Rect(randint(0, WIDTH - 50), randint(0, HEIGHT -50), 50, 50)
+
+points = list()
+for _ in range(20):
+    points.append(pygame.Rect(randint(0, WIDTH - SIZE), randint(0, HEIGHT - SIZE), SIZE, SIZE))
+
+moveDown = False
+moveUp = False
+moveLeft = False
+moveRight = False
+velocity = 5
+
 running = True
 while running:
     for event in pygame.event.get():
