ef685ea80dcfd548eeb2615420d01e0ae33a4020
Arvin Bohlool <abohloolk@gmail.com>
2025-02-03 10:44:08 +0330

    initialized

---
+++ pygame4.py
@@ -0,0 +1,13 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+
+pygame.quit()
