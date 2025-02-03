3ce9cbf69a548d0d8050996a5d729b5252777beb
Arvin Bohlool <abohloolk@gmail.com>
2025-02-03 10:45:43 +0330

    initialized

--- pygame4.py
+++ pygame4.py
@@ -3,6 +3,12 @@ from pygame.locals import *
 
 pygame.init()
 
+WIDTH = 500
+HEIGHT = 500
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("media")
+
 running = True
 while running:
     for event in pygame.event.get():
