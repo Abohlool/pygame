14e1866bbf718830539ae1b6400b25769a1d59b7
Arvin Bohlool <abohloolk@gmail.com>
2025-02-03 11:37:30 +0330

    SFX

--- pygame4.py
+++ pygame4.py
@@ -15,6 +15,11 @@ playerImg = pygame.image.load("player.png")
 playerImgFitted = pygame.transform.scale(playerImg, (40, 40))
 cherryImg = pygame.image.load("cherry.png")
 
+pickUpSound = pygame.mixer.Sound("pickup.wav")
+pygame.mixer.music.load("background.mid")
+pygame.mixer.music.play(-1, 0.0)
+musicPlaying = True
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -24,6 +29,15 @@ while running:
         if event.type == KEYUP:
             if event.key == K_ESCAPE:
                 running = False
+                
+            if event.key == K_m:
+                if musicPlaying:
+                    pygame.mixer.music.stop()
+                    
+                else:
+                    pygame.mixer.music.play(-1, 0.0)
+                    
+                musicPlaying = not musicPlaying
                 
         screen.fill(0xC7C7C7)
         pygame.display.update()
