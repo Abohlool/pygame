a6f573af1d5bcfaf06e9d8eded73251eb2d0b0c5
Arvin Bohlool <abohloolk@gmail.com>
2025-02-03 15:10:00 +0330

    player

--- pygame5.py
+++ pygame5.py
@@ -11,6 +11,10 @@ HEIGHT = 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("enemy")
 
+player = pygame.Rect(300, 100, 40, 40)
+playerImg = pygame.image.load("src8/player.png")
+playerImgFitted = pygame.transform.scale(playerImg, (player.width, player.height))
+
 running = True
 while running:
     for event in pygame.event.get():
