03476d67674a71b14d79b392e716cd60fdfa056d
Arvin Bohlool <abohloolk@gmail.com>
2025-02-05 23:11:50 +0330

    constants

--- pygame5.py
+++ pygame5.py
@@ -2,6 +2,15 @@ import pygame, sys
 from pygame.locals import *
 from random import randint
 
+WIDTH = 400
+HEIGHT = 400
+FPS = 60
+MAXSIZE = 40
+MINSIZE = 10
+MINSPEED = 1
+MAXSPEED = 8
+PLAYERSPEED = 5
+
 pygame.init()
 
 def terminate():
