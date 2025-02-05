bf40ba31ffc939e6218c0ea221e767aa9eab0d84
Arvin Bohlool <abohloolk@gmail.com>
2025-02-05 23:09:03 +0330

    schema

--- pygame5.py
+++ pygame5.py
@@ -1,24 +1,23 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
 from random import randint
 
 pygame.init()
-pygame.time.Clock()
 
-WIDTH = 400
-HEIGHT = 400 
+def terminate():
+    ...
+    
+def wait():
+    ...
+    
+def collision():
+    ...
+    
+def txt():
+    ...
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("enemy")
-
-player = pygame.Rect(300, 100, 40, 40)
-playerImg = pygame.image.load("src8/player.png")
-playerImgFitted = pygame.transform.scale(playerImg, (player.width, player.height))
-
-running = True
-while running:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            runnning = False
-
-pygame.quit()
+            terminate()
+            
